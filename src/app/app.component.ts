33381133223ba4b0516628f93b1ce17f1b7e90b6
bnanaday <bnanaday@gmail.com>
2022-03-10 19:57:06 -0600

    test product-cards are displaying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, VERSION } from '@angular/core';
+import { PRODUCTS } from './data';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  Television = PRODUCTS[0];
+  BluerayPlayer = PRODUCTS[1];
+  GameConsole = PRODUCTS[2];
+  MobilePhone = PRODUCTS[3];
 }
